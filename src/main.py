0e6c637212e79270146493867a94920e476092c7
louisfghbvc <louisfghbvc@gmail.com>
2022-03-13 17:50:08 +0800

    param

--- src/main.py
+++ src/main.py
@@ -22,6 +22,8 @@ def main():
             loader = data.Data(args)
             _model = model.Model(args, checkpoint)
             _loss = loss.Loss(args, checkpoint) if not args.test_only else None
+            print('Total params: %.4fM' % (sum(p.numel() for p in _model.parameters())/1000000.0))
+            print('Total params: %.4fM' % (sum(p.numel() for p in _model.parameters())/1000000.0), file=checkpoint.log_file)
             t = Trainer(args, loader, _model, _loss, checkpoint)
             while not t.terminate():
                 t.train()
